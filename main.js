43b9b4a51aa09cb6b5230d26e37a0c242036cc8f
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2025-03-21 12:59:23 -0500

    feat: :sparkles: End of the function part

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function checkDelimiter(str) {
     let stack = [];
     let delimiter = {
-        '(': ')',
-        '[': ']',
-        '{': '}'
+        "(": ")",
+        "[": "]",
+        "{": "}"
     };
 
     for (let i = 0; i < str.length; i++) {
         if (str[i] === "(" || str[i] === "[" || str[i] === "{") {
             stack.push(str[i]);
-        } else {
+        } else if (str[i] === ")" || str[i] === "]" || str[i] === "}") {
             let last = stack.pop();
             if (str[i] !== delimiter[last]) {
                 return false;
@@ -19,3 +19,7 @@ function checkDelimiter(str) {
 
     return stack.length === 0;
 }
+
+let text = prompt("Ingresa el texto a revisar:");
+
+console.log(checkDelimiter(text));
